c46c9c6a7143e2d0246e226fdf5067858de5b1be
Achal Aggarwal <aggarwal.achal@gmail.com>
2011-08-08 17:19:38 +0530

    added Kitchen Sink App as example project

---
+++ KitchenSink/KitchenSink/KitchenSinkAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  KitchenSinkAppDelegate.h
+//  KitchenSink
+//
+//  Created by Achal Aggarwal on 08/08/11.
+//  Copyright 2011 NA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KitchenSinkAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
