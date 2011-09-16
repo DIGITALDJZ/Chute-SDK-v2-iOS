08dcb1a8ba0a2e55e866704f04ce3c0a87b59ce6
costonb <costonb@gmail.com>
2011-09-15 17:22:55 -0700

    Modified GCEventEmailComponent
    Broke email method up into smaller methods to be able to retrieve just
    an event or just an array of email addresses.  Added a method that takes
    completion and error block.  Completion block has both event and email
    addresses for convenience.

--- GetChute/Classes/UI/GCEventEmailComponent.h
+++ GetChute/Classes/UI/GCEventEmailComponent.h
@@ -22,4 +22,9 @@
 //message and subject are optional, if dates are not supplied it will set start to 30 days ago and end to 30 days from now.
 +(MFMailComposeViewController*)MailViewControllerForEventNamed:(NSString*)event afterEventStartDate:(NSDate*)start beforeEventEndDate:(NSDate*)end withMessage:(NSString*)message andSubject:(NSString*)subject;
 
++(NSArray*)atendeeEmailsForEventNamed:(NSString*)eventName afterEventStartDate:(NSDate*)start beforeEventEndDate:(NSDate*)end;
+
++(EKEvent*)eventNamed:(NSString*)eventName afterEventStartDate:(NSDate*)start beforeEventEndDate:(NSDate*)end;
+
++(void)findAtendeesAndEventNamed:(NSString*)eventName afterEventStartDate:(NSDate*)start beforeEventEndDate:(NSDate*)end withCompletionBlock:(void (^)(EKEvent*, NSArray*))aCompletionBlock andErrorBlock:(GCErrorBlock)errorBlock;
 @end
