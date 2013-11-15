90fd65f462d48c0fd23d918fb5b4a49ae3966f08
Aleksandar Kex Trpeski <kex@aranea-apps.com>
2013-11-15 03:24:24 +0100

    Update GCConfigurationFile.m

--- Chute-SDK/Configuration/GCConfigurationFile.m
+++ Chute-SDK/Configuration/GCConfigurationFile.m
@@ -24,7 +24,7 @@ static NSDictionary *sGCServiceFeatures;
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSString *documentsDirectory = [paths objectAtIndex:0];
     NSString *path = [documentsDirectory stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.%@", kGCConfiguration, kGCExtension]];
-    NSString *bundlePath = [[NSBundle mainBundle] pathForResource:kGCConfiguration ofType:kGCExtension];;
+    NSString *bundlePath = [[NSBundle mainBundle] pathForResource:kGCConfiguration ofType:kGCExtension];
     
     NSFileManager *fileManager = [NSFileManager defaultManager];
     
